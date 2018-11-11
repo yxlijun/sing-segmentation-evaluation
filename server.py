37af7778aaeb62558e1ce69c069d79944f6c894c
lijun <2644089822@qq.com>
2018-11-11 19:59:08 +0800

    modify server.py

--- server.py
+++ server.py
@@ -33,6 +33,12 @@ class Server():
         self.zero_amp_frame = {}
         self.obs_syllable = {}
 
+        self.used_wavs = {}
+        self.flags = {}
+        self.noused_wavs = {}
+
+
+
         self._queue_socket = Queue.Queue()
         print('start receive client--------')
 
@@ -42,7 +48,7 @@ class Server():
             readable , writable , exceptional = select.select(self.inputs, self.outputs, self.inputs)
             if not (readable or writable or exceptional) :
                 break;
-            for s in readable :
+            for s in readable:
                 if s is self.server:
                     connection, client_address = s.accept()
                     connection.setblocking(0)
@@ -63,21 +69,69 @@ class Server():
                             if useid not in self.wav_files.keys():
                                 self.wav_files[useid] = list()
                                 self.pitches[useid] = np.array([])
+                                self.used_wavs[useid] = list()
+                                self.flags[useid] = list()
+                                self.noused_wavs[useid] = list()
                                 self.zero_amp_frame[useid] = np.array([])
                                 self.obs_syllable[useid] = np.array([])
 
+                            self.flags[useid].append(int(flag))
+
+                            last_wav_file = self.wav_files[useid][-1] if len(self.wav_files[useid])>0 else wav_file 
+                            cur_wav_id = int(os.path.basename(wav_file).split('.')[0])
+                            last_wav_id = int(os.path.basename(last_wav_file).split('.')[0])
+                            isused = False
+                            if (cur_wav_id - last_wav_id)==1 or (cur_wav_id - last_wav_id)==0:
+                                pitches,zero_amp_frame = self.pitch_detect(wav_file)
+                                obs_syllable = det_syllable_prob(wav_file,
+                                                                self.model_joint,
+                                                                self.scaler_joint)
+
+                                self.pitches[useid] = np.concatenate((self.pitches[useid],pitches),axis=0)
+                                self.zero_amp_frame[useid] = np.concatenate((self.zero_amp_frame[useid],zero_amp_frame),axis=0)
+                                self.obs_syllable[useid] = np.concatenate((self.obs_syllable[useid],obs_syllable),axis=0)
+                                print(wav_file,self.pitches[useid].shape,self.obs_syllable[useid].shape)
+                                self.used_wavs[useid].append(wav_file)
+                                isused = True
+                            else:
+                                used_wavs = self.used_wavs[useid]
+                                for wav in used_wavs:
+                                    wav_id =  int(os.path.basename(wav).split('.')[0])
+                                    if (cur_wav_id - wav_id == 1):
+                                        pitches,zero_amp_frame = self.pitch_detect(wav_file)
+                                        obs_syllable = det_syllable_prob(wav_file,
+                                                                        self.model_joint,
+                                                                        self.scaler_joint)
+
+                                        self.pitches[useid] = np.concatenate((self.pitches[useid],pitches),axis=0)
+                                        self.zero_amp_frame[useid] = np.concatenate((self.zero_amp_frame[useid],zero_amp_frame),axis=0)
+                                        self.obs_syllable[useid] = np.concatenate((self.obs_syllable[useid],obs_syllable),axis=0)
+                                        print(wav_file,self.pitches[useid].shape,self.obs_syllable[useid].shape)
+                                        self.used_wavs[useid].append(wav_file)
+                                        isused = True
+                            if not isused:
+                                self.noused_wavs[useid].append(wav_file)
+                                nowav_ids = [int(os.path.basename(wav).split('.')[0]) for wav in self.noused_wavs[useid]]
+                                indices = np.argsort(np.array(nowav_ids))
+                                self.noused_wavs[useid] = np.array(self.noused_wavs[useid])[indices].tolist()
+                            for nowav in self.noused_wavs[useid]:
+                                nowav_id = int(os.path.basename(nowav).split('.')[0])
+                                for wav in self.used_wavs[useid]:
+                                    wav_id =  int(os.path.basename(wav).split('.')[0])
+                                    if (nowav_id - wav_id == 1):
+                                        pitches,zero_amp_frame = self.pitch_detect(nowav)
+                                        obs_syllable = det_syllable_prob(nowav,
+                                                                        self.model_joint,
+                                                                        self.scaler_joint)
+
+                                        self.pitches[useid] = np.concatenate((self.pitches[useid],pitches),axis=0)
+                                        self.zero_amp_frame[useid] = np.concatenate((self.zero_amp_frame[useid],zero_amp_frame),axis=0)
+                                        self.obs_syllable[useid] = np.concatenate((self.obs_syllable[useid],obs_syllable),axis=0)
+                                        print(nowav,self.pitches[useid].shape,self.obs_syllable[useid].shape)
+                                        self.used_wavs[useid].append(nowav)
                             self.wav_files[useid].append(wav_file)
-                            pitches,zero_amp_frame = self.pitch_detect(wav_file)
-                            obs_syllable = det_syllable_prob(wav_file,
-                                                            self.model_joint,
-                                                            self.scaler_joint)
-
-                            self.pitches[useid] = np.concatenate((self.pitches[useid],pitches),axis=0)
-                            self.zero_amp_frame[useid] = np.concatenate((self.zero_amp_frame[useid],zero_amp_frame),axis=0)
-                            self.obs_syllable[useid] = np.concatenate((self.obs_syllable[useid],obs_syllable),axis=0)
-                            print(wav_file,self.pitches[useid].shape,self.obs_syllable[useid].shape)
-
-                            if int(flag)==1:
+                            end = (len(self.wav_files[useid])==len(self.used_wavs[useid]))
+                            if (1 in self.flags[useid]) and end:
                                 self.out_jsons[useid] = out_json
                                 self.input_jsons[useid] = input_json
                                 self.detect(useid)
@@ -131,6 +185,10 @@ class Server():
         del self.pitches[useid]
         del self.zero_amp_frame[useid]
         del self.obs_syllable[useid]
+        del self.used_wavs[useid]
+        del self.flags[useid]
+        del self.noused_wavs[useid]
+
 
 if __name__ == "__main__":
     s = Server()
