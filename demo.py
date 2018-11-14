4c3e2391cd21ddca2a166ec9b0495b4095cdae51
lijun <2644089822@qq.com>
2018-11-14 08:00:15 +0800

    modify demo

--- demo.py
+++ demo.py
@@ -4,11 +4,12 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-from utils import parse_musescore
 from alignment import sw_alignment
 from pitchDetection.mfshs import MFSHS
-from onset_predict import detector_onset
 from post_process import Evaluator,trans_onset_and_offset
+from utils import parse_musescore,get_wav_and_json_file,save_files
+from onset_predict import detector_onset,load_cnn_model,det_syllable_prob
+
 
 
 def _main(wav_file,input_json,output_json):
@@ -19,22 +20,37 @@ def _main(wav_file,input_json,output_json):
 	mfshs.pitch_detector()
 	pitches = mfshs.pitches
 	zero_amp_frame = mfshs.zeroAmploc
-
-	score_note,pauseLoc = parse_musescore(input_json)
+	score_note,note_types,pauseLoc = parse_musescore(input_json)  ## parse musescore
 	'''
 		second detect onset 
 	'''
-	onset_frame = detector_onset(wav_file,pitches,score_note)
+	model_joint,scaler_joint = load_cnn_model()
+	obs_syllable = det_syllable_prob(wav_file,
+									model_joint,
+									scaler_joint)
+	onset_frame = detector_onset(obs_syllable,
+								 pitches,
+								 score_note)
 	'''
 		sw alignment
 	'''
-	match_loc_info = sw_alignment(pitches,onset_frame,score_note)
+	match_loc_info = sw_alignment(pitches,
+								  onset_frame,
+								  score_note)
+
 
 	'''
 		post process and save result
 	'''
-	onset_offset_pitches = trans_onset_and_offset(match_loc_info,onset_frame,pitches)	
-	evaluator = Evaluator(output_json,onset_offset_pitches,zero_amp_frame,score_note,pauseLoc)
+	onset_offset_pitches = trans_onset_and_offset(match_loc_info,
+												  onset_frame,
+												  pitches)
+	evaluator = Evaluator(output_json,
+						 onset_offset_pitches,
+						 zero_amp_frame,
+						 score_note,
+						 pauseLoc,
+						 note_types)
 
 
 if __name__ == '__main__':
